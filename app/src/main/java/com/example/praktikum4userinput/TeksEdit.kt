cb29e4ce024ea4c40e677903992bee063d32036a
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-10-31 18:42:42 +0700

    Menambahkan textNama, textAlamat, dan textJK

--- app/src/main/java/com/example/praktikum4userinput/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4userinput/TeksEdit.kt
@@ -1,9 +1,17 @@
 package com.example.praktikum4userinput
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormPendaftaran(modifier: Modifier = Modifier) {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
 
 }
