ac6f00b40b151d7e13ee550df69448c70d215c9c
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-10-31 19:06:15 +0700

    menambahkan teks judul “Formulir Pendaftaran"

--- app/src/main/java/com/example/praktikum4userinput/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4userinput/TeksEdit.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,7 +16,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FormPendaftaran(modifier: Modifier = Modifier) {
@@ -52,7 +55,12 @@ fun FormPendaftaran(modifier: Modifier = Modifier) {
                 .padding(vertical = 25.dp),
             contentAlignment = Alignment.Center
         ){
-
+            Text(
+                text = "Formulir Pendaftaran",
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
 
     }
